f5a32956a4e4b9fc0dedd666f71ea49f4a05e8f5
otondi-willis <156656236+otondi-willis@users.noreply.github.com>
2024-06-13 17:12:07 +0000

    changes to enable withdrawal and deposit transactions reflect in the database as well. Added amount exception as well for error handling

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -7,63 +7,74 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
-  public static Connection connect(){
+  public static Connection connect() {
     String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
-    
-    try{
+
+    try {
       connection = DriverManager.getConnection(db_file);
       System.out.println("we are connected");
-    } catch(SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return connection;
-    
+
   }
-  public static Customer getCustomer(String username){
+
+  public static Customer getCustomer(String username) {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
-    try(Connection connection = connect();
-    PreparedStatement statement = connection.prepareStatement(sql)){
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
       statement.setString(1, username);
-      try (ResultSet resultSet = statement.executeQuery()){
-         customer = new Customer(
-          resultSet.getInt ("id"), 
-          resultSet.getString ("name"),
-          resultSet.getString ("username"), 
-          resultSet.getString ("password"),
-          resultSet.getInt ("account_id"));
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
 
-          
       }
-    }catch (SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return customer;
   }
-  public static Account getAccount(int accountId){
+
+  public static Account getAccount(int accountId) {
     String sql = "select * from accounts where id = ?";
     Account account = null;
-    try(Connection connection = connect();
-    PreparedStatement statement = connection.prepareStatement(sql)){
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
       statement.setInt(1, accountId);
-      try(ResultSet resultSet = statement.executeQuery()){
+      try (ResultSet resultSet = statement.executeQuery()) {
         account = new Account(
-          resultSet.getInt("id"),
-          resultSet.getString("type"),
-          resultSet.getDouble("balance")
-        );
-      } 
-      
-    } catch (SQLException e){
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return account;
   }
-  public static void main(String[] args) {
-    Customer customer = getCustomer("dbiddelljy@privacy.gov.au");
-    Account account = getAccount(customer.getAccountId());
-    System.out.println(account.getBalance());
-    
+
+  public static void updateAccountBalance(int accountId, double balance) {
+    String sql = "update accounts set balance = ? where id = ?";
+    try (
+        Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+    ) {
+      statement.setDouble(1, balance);
+      statement.setInt(2, accountId);
+
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
+
 }
